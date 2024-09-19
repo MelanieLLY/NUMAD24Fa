1e7d9fcaf1ec53b47645f31e961364ca219ab4e0
MelanieLLY <105343088+MelanieLLY@users.noreply.github.com>
2024-09-19 15:33:26 -0700

    A1 upload

---
+++ app/src/main/java/edu/northeastern/numad24fa_liuyiyang/MainActivity.java
@@ -0,0 +1,38 @@
+package edu.northeastern.numad24fa_liuyiyang;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button toastButton = findViewById(R.id.toastButton);
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "my name is Liuyi Yang and my email is yang.liuyi@northeastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
